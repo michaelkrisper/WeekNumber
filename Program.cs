dac3d728fdf73316d282ecb49d0b9a09c2b927c4
Michael Krisper <michael.krisper@gmail.com>
2025-09-23 16:48:12 +0200

    rearranged code

--- Program.cs
+++ Program.cs
@@ -41,18 +41,16 @@ internal class Program : ApplicationContext
         var smallFontSize = Math.Max(4, iconSize * 0.5f);
         var bigFontSize = Math.Max(6, iconSize * 0.7f);
 
-        using var bitmap = new Bitmap(iconSize, iconSize);
-        using var graphics = Graphics.FromImage(bitmap);
-
         using var fontSmall = new Font("Segoe UI", smallFontSize, FontStyle.Regular, GraphicsUnit.Pixel);
         using Brush brush = new SolidBrush(Color.White);
         using var fontBig = new Font("Segoe UI", bigFontSize, FontStyle.Bold, GraphicsUnit.Pixel);
         using var format = new StringFormat { Alignment = StringAlignment.Center };
-
+        using var bitmap = new Bitmap(iconSize, iconSize);
+        using var graphics = Graphics.FromImage(bitmap);
         graphics.DrawString("KW", fontSmall, brush, iconSize / 2f, -smallFontSize / 2f, format);
         graphics.DrawString(week, fontBig, brush, iconSize / 2f, fontSmall.Height - smallFontSize, format);
-
         using var icon = Icon.FromHandle(bitmap.GetHicon());
+        
         var context = new ContextMenu([new MenuItem("Exit", (_, __) => Application.Exit())]);
         _ = new NotifyIcon { Visible = true, ContextMenu = context, Icon = icon, Text = "KW " + week };
 
